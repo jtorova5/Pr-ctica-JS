92b12ca9bcc4d863357cd3ed453495b9a2c6d0fd
jtorova5 <jtorova5@gmail.com>
2024-04-20 09:12:22 -0500

    Se suben los primeros 19 ejercicios de las sesión 05_EstructurasDeControl

--- 05_Ejercicios/src/js/main.js
+++ 05_Ejercicios/src/js/main.js
@@ -4,11 +4,13 @@
 let num = 20
 
 console.groupCollapsed('Ejercicio 1: ')
+
 if (num > 0) {
     console.log(`El número ${num} es positivo`)
 } else {
     console.log(`El número ${num} es negativo`)
 }
+
 console.groupEnd()
 
 // 2. Verifica si un número es negativo
@@ -16,11 +18,13 @@ console.groupEnd()
 let num2 = -20
 
 console.groupCollapsed('Ejercicio 2: ')
+
 if (num2 < 0) {
     console.log(`El número ${num2} es negativo`)
 } else {
     console.log(`El número ${num2} es positivo`)
 }
+
 console.groupEnd()
 
 // 3. Comprueba si un número es par
@@ -29,11 +33,13 @@ let num3 = 80
 let parTest = num3 % 2
 
 console.groupCollapsed('Ejercicio 3: ')
+
 if (parTest == 0) {
     console.log(`El número ${num3} es par`)
 } else {
     console.log(`El número ${num3} es impar`)
 }
+
 console.groupEnd()
 
 // 4. Comprueba si un número es impar
@@ -42,11 +48,13 @@ let num4 = 33
 let imparTest = num4 % 2
 
 console.groupCollapsed('Ejercicio 4: ')
+
 if (imparTest != 0) {
     console.log(`El número ${num4} es impar`)
 } else {
     console.log(`El número ${num4} es par`)
 }
+
 console.groupEnd()
 
 // 5. Determina si un número es múltiplo de 5
@@ -56,11 +64,13 @@ let multiploDe = 5
 let pruebaMultiplo = num5 % multiploDe
 
 console.groupCollapsed('Ejercicio 5: ')
+
 if (pruebaMultiplo == 0) {
     console.log(`El número ${num5} es múltiplo de ${multiploDe}`)
 } else {
     console.log(`El número ${num5} NO es múltiplo de ${multiploDe}`)
 }
+
 console.groupEnd()
 
 // 6. Verifica si un número es divisible entre 3
@@ -70,11 +80,13 @@ let divisiblePor = 3
 let pruebaDivisible = num6 % divisiblePor
 
 console.groupCollapsed('Ejercicio 6: ')
+
 if (pruebaDivisible == 0) {
     console.log(`El número ${num6} es divisible por ${divisiblePor}`)
 } else {
     console.log(`El número ${num6} NO es divisible por ${divisiblePor}`)
 }
+
 console.groupEnd()
 
 // 7. Determina si un número es mayor que 100
@@ -83,11 +95,13 @@ let num7 = 101
 let limite = 100
 
 console.groupCollapsed('Ejercicio 7: ')
+
 if (num7 > limite) {
     console.log(`El número ${num7} es mayor que ${limite}`)
 } else {
     console.log(`El número ${num7} es menor que ${limite}`)
 }
+
 console.groupEnd()
 
 // 8. Verifica si un número es menor que -50 
@@ -96,11 +110,13 @@ let num8 = -60
 let limite2 = -50
 
 console.groupCollapsed('Ejercicio 8: ')
+
 if (num8 < limite2) {
     console.log(`El número ${num8} es menor que ${limite2}`)
 } else {
     console.log(`El número ${num8} es mayor que ${limite2}`)
 }
+
 console.groupEnd()
 
 // 9. Comprueba si un número está en el rango de 20 a 50
@@ -110,11 +126,13 @@ let limiteInf = 20
 let limiteSup = 50
 
 console.groupCollapsed('Ejercicio 9: ')
+
 if ((num9 > limiteInf) && (num9 < limiteSup)) {
     console.log(`El número ${num9} se encuentra en el rango (${limiteInf}, ${limiteSup})`)
 } else {
     console.log(`El número ${num9} NO se encuentra en el rango (${limiteInf}, ${limiteSup})`)
 }
+
 console.groupEnd()
 
 // 10. Determina si un número es igual a 0
@@ -123,11 +141,13 @@ let num10 = 0
 let test = 0
 
 console.groupCollapsed('Ejercicio 10: ')
+
 if (num10 == test) {
     console.log(`El ${num10} es igual a ${test}`)
 } else {
     console.log(`El ${num10} NO es igual a ${test}`)
 }
+
 console.groupEnd()
 
 // 11. Verifica si un número es mayor que -10 y menor que 10
@@ -137,17 +157,28 @@ let limiteInf2 = -10
 let limiteSup2 = 10
 
 console.groupCollapsed('Ejercicio 11: ')
+
 if ((num11 > limiteInf2) && (num11 < limiteSup2)) {
     console.log(`El número ${num11} es mayor que ${limiteInf2} y menor que ${limiteSup2}`)
 } else {
     console.log(`El número ${num11} NO se encuentra en el rango dado`)
-    
 }
+
 console.groupEnd()
 
 // 12. Determina si un número es un año bisiesto
 
+let año = 2020
+let testBisiesto = ((año % 4 ==  0) || ((año %  100 == 0) && (año % 400 == 0)))
+
 console.groupCollapsed('Ejercicio 12: ')
+
+if (testBisiesto) {
+    console.log(`El año ${año} es bisiesto`)
+} else {
+    console.log(`El año ${año} NO es bisiesto`)
+}
+
 console.groupEnd()
 
 // 13. Verifica si una persona es mayor de edad (mayor o igual a 18 años)
@@ -156,67 +187,106 @@ let num12 = 17
 let test2 = 18
 
 console.groupCollapsed('Ejercicio 13: ')
+
 if (num12 >= test2) {
     console.log(`Tienes ${num12} años, eres mayor de edad`)
 } else {
     console.log(`No eres mayor de edad, debes cumplir ${test2} años para eso`)
 }
+
 console.groupEnd()
 
 // 15. Verifica si un número es un cuadrado perfecto
 
-let num13 = 25
+let num13 = 36
 let raiz = Math.sqrt(num13)
-let cuadrado = raiz ** 2
-let testCuadradoPerfecto = (cuadrado == num13)
+let raizRedondeada = Math.round(raiz)
+let testCuadradoPerfecto = (raiz == raizRedondeada)
 
 console.groupCollapsed('Ejercicio 15: ')
+
 if (testCuadradoPerfecto) {
     console.log(`El número ${num13} es un cuadrado perfecto y su raiz es ${raiz}`)
 } else {
     console.log(`El número ${num13} NO es un cuadrado perfecto`)
 }
+
 console.groupEnd()
 
 // 16. Determina si un número es un número de Fibonacci (Es la suma de los dos números anteriores)
 
-let num14 = 5
-let testFibonacci = (num14 == ((num14 - 1) + (num14 - 2)))
+let num14 = 8
+let a = 0
+let b = 1
+let testFibonacci = false
+
+while (b < num14) {
+    let temp = b;
+    b = a + b;
+    a = temp;
+}
+
+if (b === num14) {
+    testFibonacci = true;
+}
 
 console.groupCollapsed('Ejercicio 16: ')
+
 if (testFibonacci) {
-    console.log(`${num14} = (${num14 - 1} + ${num14 - 2}), por lo tanto es un número de Fibonacci`)
+    console.log(`${num14} es un número de Fibonacci`)
 } else {
-    console.log(`No es un número de Fibonacci`)
+    console.log(`${num14} NO es un número de Fibonacci`)
 }
+
 console.groupEnd()
 
 // 17. Verifica si un número es una potencia de 2. 
 
-let num15 = 144
+let num15 = 35
 let raiz2 = Math.sqrt(num15)
-let potencia = raiz2 ** 2
-let testPotencia = (potencia == num15)
+let raizRedondeada2 = Math.round(raiz2)
+let testPotencia = (raiz2 == raizRedondeada2)
 
 console.groupCollapsed('Ejercicio 17: ')
+
 if (testPotencia) {
     console.log(`El número ${num15} es potencia de 2 y su raíz es ${raiz2}`)
 } else {
     console.log(`El número ${num15} NO es potencia de 2`)
 }
+
 console.groupEnd()
 
 // 18. Determina si un número es un palíndromo. 
 
 let num16 = 112211
-let numString = String(num16)
-let testPalindromo = (num16 == (numString).)
-console.groupCollapsed('Ejercicio 1: ')
+let numString = (num16.toString()).split('').reverse().join('')
+let testPalindromo = (num16 == numString)
+
+console.groupCollapsed('Ejercicio 18: ')
+
+if (testPalindromo) {
+    console.log(`${num16} es un palíndromo ya que ${num16} = ${numString}`);
+} else {
+    console.log(`${num16} NO es un palíndromo ya que ${num16} != ${numString}`);
+}
+
 console.groupEnd()
+
 // 19. Verifica si una cadena de texto contiene la palabra "JavaScript". 
 
-console.groupCollapsed('Ejercicio 1: ')
+let cadena = 'Bienvenidos al curso de JavaScript para el año 2024'
+let palabra = 'JavaScript'
+let testCadena = cadena.includes(palabra)
+
+console.groupCollapsed('Ejercicio 19: ')
+if (testCadena) {
+    console.log(`La cadena de texto "${cadena}" contiene la palabra ${palabra}`)
+} else {
+    console.log(`La cadena de texto "${cadena}" NO contiene la palabra ${palabra}`)
+}
 console.groupEnd()
+
 // 20. Determina si una cadena tiene más de 10 caracteres. 
 
 console.groupCollapsed('Ejercicio 1: ')
